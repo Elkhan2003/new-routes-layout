ebab8999a9b837b696a004d696c62ddfac2906b6
Elcho911 <boss.armsport@gmail.com>
2024-06-02 09:38:27 +0600

    added files to github

---
+++ src/pagesSide/components/layout/LayoutSide.tsx
@@ -0,0 +1,9 @@
+import scss from "./LayoutSide.module.scss";
+
+const LayoutSide = () => {
+	return <div className={scss.LayoutSide}>
+    
+  </div>;
+};
+
+export default LayoutSide;
---
+++ src/pagesSide/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Feedback from "./homeSections/Feedback";
+import Welcome from "./homeSections/Welcome";
+
+const HomePage = () => {
+	return (
+		<>
+			<Welcome />
+			<Feedback />
+		</>
+	);
+};
+
+export default HomePage;
